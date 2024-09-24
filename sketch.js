9197059e9ff04194c903b38e48b836ed6014b919
rafinhaplayer <rafaela.reversi.soares@escola.pr.gov.br>
2024-09-24 10:33:01 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  fill(135, 206, 235)
+  rect(0, 0, 400, 200)
+fill(255, 223, 0)
+  circle(230, 200, 80)
+  fill(0, 105, 148)
+  rect(0, 200, 400, 200)
+}
